5172b66f5fce65b28a0a4e1edad24065913d10e3
SlavBorowski <slav.borowski@outlook.com>
2020-11-11 17:07:50 +1100

    Added new trip form.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,11 +5,16 @@ class TripsController < ApplicationController
   end
   
   def new
-
+    @trip = Trip.new
   end
   
   def create
-
+    @trip = Trip.new
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end    
   end
   
   def update
