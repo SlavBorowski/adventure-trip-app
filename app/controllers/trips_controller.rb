86ba407cb28d8775bb463823120a9a31820a04c8
SlavBorowski <slav.borowski@outlook.com>
2020-11-11 16:14:44 +1100

    Added resource methods to trips controller.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,23 @@
 class TripsController < ApplicationController
+
+  def index
+
+  end
+  
+  def new
+
+  end
+  
+  def create
+
+  end
+  
+  def update
+
+  end
+  
+  def delete
+
+  end  
+
 end
