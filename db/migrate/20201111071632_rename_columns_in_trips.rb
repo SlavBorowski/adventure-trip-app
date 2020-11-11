6b828bb9bd68e481ee974a894745fa4dc7e983f4
SlavBorowski <slav.borowski@outlook.com>
2020-11-11 18:18:33 +1100

    Renamed 2 columns in Trips table.

---
+++ db/migrate/20201111071632_rename_columns_in_trips.rb
@@ -0,0 +1,6 @@
+class RenameColumnsInTrips < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :trips, :startdate, :start_date
+    rename_column :trips, :enddate, :end_date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_041422) do
+ActiveRecord::Schema.define(version: 2020_11_11_071632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2020_11_11_041422) do
     t.string "title"
     t.integer "duration"
     t.string "description"
-    t.date "startdate"
-    t.date "enddate"
+    t.date "start_date"
+    t.date "end_date"
     t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
