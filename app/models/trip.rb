9a193ee06a36e50a52ab8a289b10165c95eded1c
SlavBorowski <slav.borowski@outlook.com>
2020-11-12 17:21:10 +1100

    Updated relationship between trip and activity.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
   has_one :address
+  belongs_to :activity
   accepts_nested_attributes_for :address
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_054802) do
+ActiveRecord::Schema.define(version: 2020_11_12_055116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,10 @@ ActiveRecord::Schema.define(version: 2020_11_12_054802) do
     t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "activity_id", null: false
+    t.index ["activity_id"], name: "index_trips_on_activity_id"
   end
 
   add_foreign_key "addresses", "trips"
+  add_foreign_key "trips", "activities"
 end
