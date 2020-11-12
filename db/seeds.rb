7a4299ac7649d3836180dda70086264ee3034cca
SlavBorowski <slav.borowski@outlook.com>
2020-11-12 17:53:19 +1100

    Created seeds fot activity table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+activities = Activity.create([{ activity_type: 'Hiking' }, {activity_type: 'Kayaking' }, { activity_type: 'Canoeing' }, { activity_type: 'Rock Climbing' }, { activity_type: 'Abseiling' }, { activity_type: 'Horse Riding' }, { activity_type: 'Scuba Diving' }, { activity_type: 'Bike Riding' }, { activity_type: 'Rafting' }, { activity_type: 'Sailing' }, { activity_type: 'Snorkeling' }, { activity_type: 'Surfing' }])
+
