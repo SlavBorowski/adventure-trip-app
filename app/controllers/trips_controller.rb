80b765fce3094615b2f56633ec71371f462bf57a
SlavBorowski <slav.borowski@outlook.com>
2020-11-12 14:29:49 +1100

    Updated New view to take address attributes for trip.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,6 +6,7 @@ class TripsController < ApplicationController
   
   def new
     @trip = Trip.new
+    @trip.build_address
   end
   
   def create
@@ -29,7 +30,7 @@ class TripsController < ApplicationController
   private
 
   def trip_params
-    params.require(:trip).permit(:title, :duration, :description, :startdate, :enddate, :price)
+    params.require(:trip).permit(:title, :duration, :description, :start_date, :end_date, :price, address_attributes: [ :addr1, :addr2, :city, :state, :postcode ])
   end  
 
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,4 @@
 class Trip < ApplicationRecord
   has_one :address
+  accepts_nested_attributes_for :address
 end
