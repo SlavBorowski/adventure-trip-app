16198f84d45bc89446d0de8206026ff3082c0c95
SlavBorowski <slav.borowski@outlook.com>
2020-11-16 10:57:45 +1100

    Updated routes for buyers and sellers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   
-  devise_for :buyers, path: 'buyers'
-  devise_for :sellers, path: 'sellers'
+  #devise_for :buyers, path: 'buyers'
+  #devise_for :sellers, path: 'sellers'
+  devise_for :buyers, path: 'buyers', controllers: { sessions: "buyers/sessions" }
+  devise_for :sellers, path: 'sellers', controllers: { sessions: "sellers/sessions" }
   root to: "trips#index"
   
   resources :trips, only: [:index, :new, :create, :show, :update, :delete]
+ 
 end
