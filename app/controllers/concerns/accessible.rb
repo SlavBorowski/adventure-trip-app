06bb5429e8ccd6a836abb74d136f74bf2ac6589e
SlavBorowski <slav.borowski@outlook.com>
2020-11-16 12:39:22 +1100

    Added navbar to application.html.erb

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -9,11 +9,11 @@ module Accessible
     if current_seller
       flash.clear
       # if you have rails_admin. You can redirect anywhere really
-      redirect_to(rails_seller.dashboard_path) and return
+      redirect_to(trips_path) and return
     elsif current_buyer
       flash.clear
       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_buyer_root_path) and return
+      redirect_to(trips_path) and return
     end
   end
 end
