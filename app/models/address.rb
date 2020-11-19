58bdf6bba6dd3231e2490169cee8accd397b0121
SlavBorowski <slav.borowski@outlook.com>
2020-11-19 11:39:37 +1100

    Added address validations.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :trip
+
+  validates :addr1, presence: :true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postcode, presence: true, length: { is: 4 }, numericality: { greater_than: 0 }
 end
