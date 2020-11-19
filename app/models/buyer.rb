320dacf16db396b4521c3470942dd5b9a278452c
SlavBorowski <slav.borowski@outlook.com>
2020-11-19 11:11:33 +1100

    Added buyer validations.

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,5 +2,8 @@ class Buyer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+  validates :first_name, presence: true, length: { minimum: 1 }
+  validates :last_name, presence: true, length: { minimum: 1 }
+  validates :phone, presence: true, length: { is: 10 }
+  
 end
